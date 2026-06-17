66db92dea348931e3b40b00341e14d0895649d67
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-17 11:56:14 +0300

    Create EventTest.java

---
+++ src/test/java/hexlet/pet/EventTest.java
@@ -0,0 +1,50 @@
+package hexlet.pet;
+
+import hexlet.pet.calendar.Event;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventTest {
+
+    @Test
+    public void testEvent() {
+        var testName = "this is a test name";
+        var date = LocalDateTime.now();
+        Event event = new Event(testName, date);
+        assertEquals(testName, event.getName());
+        assertEquals(date, event.getDate());
+
+        var id = event.getId();
+        var toString = testName + " (ID: " + id + ") - " + date;
+        assertEquals(toString, event.toString());
+    }
+
+    @Test
+    public void testEvent2() {
+        String name = null;
+        LocalDateTime date = null;
+        Event event = new Event(name, date);
+        assertEquals(name, event.getName());
+        assertEquals(date, event.getDate());
+
+        var id = event.getId();
+        var toString = name + " (ID: " + id + ") - " + date;
+        assertEquals(toString, event.toString());
+    }
+    
+    @Test
+    public void testEvent3() {
+        String name = "";
+        LocalDateTime date = null;
+        Event event = new Event(name, date);
+        assertEquals(name, event.getName());
+        assertEquals(date, event.getDate());
+
+        var id = event.getId();
+        var toString = name + " (ID: " + id + ") - " + date;
+        assertEquals(toString, event.toString());
+    }
+}
