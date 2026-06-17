0fd4975b25620decf4e8a837c98fa2f678924268
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-17 15:52:20 +0300

    Update AppTest.java

--- src/test/java/hexlet/pet/AppTest.java
+++ src/test/java/hexlet/pet/AppTest.java
@@ -38,7 +38,7 @@ public class AppTest {
         String[] args = {"-a", "-n", name, "-d", date.toString(), "-f", filePath};
 
         int exitCode = new CommandLine(new App()).execute(args);
-        Assertions.assertEquals(0, exitCode);
+        assertEquals(0, exitCode);
 
         Calendar calendar = new Calendar(filePath);
         List<Event> events = calendar.getUpcoming(5);
@@ -62,11 +62,11 @@ public class AppTest {
     }
 
     @Test
-    public void testAddEvent3() throws IOException {
+    public void testAddEvent3() {
         String name3 = null;
         String[] args3 = {"-a", "-n", name3, "-d", null, "-f", filePath};
         int exitCode3 = new CommandLine(new App()).execute(args3);
-        Assertions.assertEquals(1, exitCode3);
+        assertEquals(1, exitCode3);
     }
 
     @Test
@@ -76,7 +76,7 @@ public class AppTest {
         String[] args = {"-a", "-n", name, "-d", date.toString(), "-f", filePath};
 
         int exitCode = new CommandLine(new App()).execute(args);
-        Assertions.assertEquals(0, exitCode);
+        assertEquals(0, exitCode);
 
         Calendar calendar = new Calendar(filePath);
         List<Event> events = calendar.getUpcoming(5);
@@ -91,36 +91,111 @@ public class AppTest {
     }
 
     @Test
-    public void testRemoveBy() throws IOException {
+    public void testRemoveById() throws IOException {
         var name = "my test upcoming event";
         var date = LocalDate.now().plusMonths(1);
         String[] args = {"-a", "-n", name, "-d", date.toString(), "-f", filePath};
         int exitCode = new CommandLine(new App()).execute(args);
-        Assertions.assertEquals(0, exitCode);
-
-        var name2 = "my test past event";
-        var date2 = LocalDate.now().minusMonths(1);
-        String[] args2 = {"-a", "-n", name2, "-d", date2.toString(), "-f", filePath};
-        int exitCode2 = new CommandLine(new App()).execute(args2);
-        Assertions.assertEquals(0, exitCode2);
+        assertEquals(0, exitCode);
 
         Calendar calendar = new Calendar(filePath);
         List<Event> upcomingEvents = calendar.getUpcoming(5);
-        List<Event> pastEvents = calendar.getPast(5);
         assertEquals(1, upcomingEvents.size());
-        assertEquals(1, pastEvents.size());
 
         var id  = upcomingEvents.get(0).getId();
         String[] args3 = {"-r", id, "-f", filePath};
         int exitCode3 = new CommandLine(new App()).execute(args3);
-        Assertions.assertEquals(0, exitCode3);
+        assertEquals(0, exitCode3);
         upcomingEvents = calendar.getUpcoming(5);
         assertEquals(0, upcomingEvents.size());
 
-        String[] args4 = {"-rn", name2, "-f", filePath};
-        int exitCode4 = new CommandLine(new App()).execute(args4);
-        Assertions.assertEquals(0, exitCode4);
         upcomingEvents = calendar.getUpcoming(5);
         assertEquals(0, upcomingEvents.size());
     }
+
+    @Test
+    public void testRemoveByName() throws IOException {
+        var name2 = "my test past event";
+        var date2 = LocalDate.now().minusMonths(1);
+        String[] args2 = {"-a", "-n", name2, "-d", date2.toString(), "-f", filePath};
+        int exitCode2 = new CommandLine(new App()).execute(args2);
+        assertEquals(0, exitCode2);
+
+        Calendar calendar = new Calendar(filePath);
+        List<Event> pastEvents = calendar.getPast(5);
+        assertEquals(1, pastEvents.size());
+
+        String[] args4 = {"-rn", name2, "-f", filePath};
+        int exitCode4 = new CommandLine(new App()).execute(args4);
+        assertEquals(0, exitCode4);
+
+        pastEvents = calendar.getPast(5);
+        assertEquals(0, pastEvents.size());
+
+        String removeName = "I don't exist";
+        String[] args = {"-rn", removeName , "-f", filePath};
+    }
+
+    @Test
+    public void testShow() throws IOException {
+        String[] names = {"my test upcoming event", "my test upcoming event2", "my test upcoming event3"};
+        LocalDate[] dates = {LocalDate.now().plusMonths(1), LocalDate.now().plusMonths(2),
+                LocalDate.now().plusMonths(3)};
+
+        for (int i = 0; i < 3; i++) {
+            String[] args = {"-a", "-n", names[i], "-d", dates[i].toString(), "-f", filePath};
+            int exitCode = new CommandLine(new App()).execute(args);
+            assertEquals(0, exitCode);
+        }
+
+        Calendar calendar = new Calendar(filePath);
+        List<Event> events = calendar.getUpcoming(5);
+        assertEquals(3, events.size());
+
+        String[] args2 = {"-s", "3", "-f", filePath};
+        int exitCode2 = new CommandLine(new App()).execute(args2);
+        assertEquals(0, exitCode2);
+    }
+
+    @Test
+    public void testPast() throws IOException {
+        String[] names = {"my test past event", "my test past event2", "my test past event3"};
+        LocalDate[] dates = {LocalDate.now().minusMonths(1), LocalDate.now().minusMonths(2),
+                LocalDate.now().minusMonths(3)};
+
+        for (int i = 0; i < 3; i++) {
+            String[] args = {"-a", "-n", names[i], "-d", dates[i].toString(), "-f", filePath};
+            int exitCode = new CommandLine(new App()).execute(args);
+            assertEquals(0, exitCode);
+        }
+
+        Calendar calendar = new Calendar(filePath);
+        List<Event> pastEvents = calendar.getPast(5);
+        assertEquals(3, pastEvents.size());
+
+        String[] args2 = {"-p", "3", "-f", filePath};
+        int exitCode2 = new CommandLine(new App()).execute(args2);
+        assertEquals(0, exitCode2);
+    }
+
+//     Эти два теста на данный момент не рабочие, но они добавлены, мб потом перепишу App
+//    @Test
+//    public void testRemoveByName2() throws IOException {
+//        App app = new App();
+//        String removeName = "I don't exist";
+//        String[] args = {"-rn", removeName , "-f", filePath};
+//        new CommandLine(new App()).execute(args);
+//
+//        assertThrows(Exception.class, app::run);
+//    }
+//
+//    @Test
+//    public void testRemoveById2() throws IOException {
+//        App app = new App();
+//        String removeId = "I don't exist";
+//        String[] args = {"-r", removeId, "-f", filePath};
+//        new CommandLine(new App()).execute(args);
+//
+//        assertThrows(Exception.class, app::run);
+//    }
 }
