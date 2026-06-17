24955e4e782db1008e4ff4cd52b741c41df3659b
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-17 14:39:56 +0300

    Update AppTest.java

--- src/test/java/hexlet/pet/AppTest.java
+++ src/test/java/hexlet/pet/AppTest.java
@@ -44,9 +44,10 @@ public class AppTest {
         List<Event> events = calendar.getUpcoming(5);
         assertEquals(1, events.size());
         assertEquals(name, events.get(0).getName());
+    }
 
-
-
+    @Test
+    public void testAddEvent2() throws IOException {
         var name2 = "my test past event";
         var date2 = LocalDate.now().minusMonths(1);
         String[] args2 = {"-a", "-n", name2, "-d", date2.toString(), "-f", filePath};
@@ -58,5 +59,34 @@ public class AppTest {
         List<Event> events2 = calendar2.getPast(5);
         assertEquals(1, events2.size());
         assertEquals(name2, events2.get(0).getName());
+    }
+
+    @Test
+    public void testAddEvent3() throws IOException {
+        String name3 = null;
+        String[] args3 = {"-a", "-n", name3, "-d", null, "-f", filePath};
+        int exitCode3 = new CommandLine(new App()).execute(args3);
+        Assertions.assertEquals(1, exitCode3);
+    }
+
+    @Test
+    public void testClear() throws IOException {
+        var name = "my test upcoming event";
+        var date = LocalDate.now().plusMonths(1);
+        String[] args = {"-a", "-n", name, "-d", date.toString(), "-f", filePath};
+
+        int exitCode = new CommandLine(new App()).execute(args);
+        Assertions.assertEquals(0, exitCode);
+
+        Calendar calendar = new Calendar(filePath);
+        List<Event> events = calendar.getUpcoming(5);
+        assertEquals(1, events.size());
+
+        String[] args2 = {"-c", "-f", filePath};
+        int exitCode2 = new CommandLine(new App()).execute(args2);
+        Assertions.assertEquals(0, exitCode2);
+
+        events = calendar.getUpcoming(3);
+        assertEquals(0, events.size());
     }
 }
