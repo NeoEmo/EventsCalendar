6197eeaebcb254f77ffbbef6fc3bd617d90f5ae4
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-17 11:35:38 +0300

    update README

--- src/main/java/hexlet/pet/calendar/Calendar.java
+++ src/main/java/hexlet/pet/calendar/Calendar.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Calendar {
     private final ObjectMapper mapper;
