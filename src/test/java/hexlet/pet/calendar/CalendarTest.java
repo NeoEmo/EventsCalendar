d16b21baf58fce13aef0380d6b0826a57746766b
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-17 13:03:59 +0300

    replace CalendarTest and EventTest

--- src/test/java/hexlet/pet/calendar/CalendarTest.java
+++ src/test/java/hexlet/pet/calendar/CalendarTest.java
@@ -1,6 +1,5 @@
-package hexlet.pet;
+package hexlet.pet.calendar;
 
-import hexlet.pet.calendar.Calendar;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
