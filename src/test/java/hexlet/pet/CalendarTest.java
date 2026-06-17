972825baad2ba6336a465665458cc0424c114e39
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-17 12:55:20 +0300

    create CalendarTest

---
+++ src/test/java/hexlet/pet/CalendarTest.java
@@ -0,0 +1,62 @@
+package hexlet.pet;
+
+import hexlet.pet.calendar.Calendar;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class CalendarTest {
+    private Calendar calendar;
+    private Path tempFile;
+
+    @BeforeEach
+    public void setup() throws IOException {
+        tempFile = Files.createTempFile("Calendar", ".json");
+        calendar = new Calendar(tempFile.toString());
+    }
+
+    @AfterEach
+    public void teardown() throws IOException {
+        Files.deleteIfExists(tempFile);
+    }
+
+    @Test
+    public void testAddEvent() throws IOException {
+        var name = "my test name";
+        var date =  LocalDateTime.now();
+
+        assertTrue(calendar.add(name, date));
+        assertTrue(calendar.clear());
+    }
+
+    @Test
+    public void testRemoveBy() throws IOException {
+        var name = "my test name";
+        var date =  LocalDateTime.now();
+
+        var name2 = "my test name2";
+        var date2 =  LocalDateTime.now().minusDays(1);
+
+        calendar.add(name, date);
+        calendar.add(name2, date2);
+
+        String id = calendar.getIdByName(name).orElseThrow();
+        assertEquals(Optional.empty(), calendar.getIdByName(name+name2));
+        assertTrue(calendar.removeById(id));
+        assertFalse(calendar.removeById(id));
+
+        assertTrue(calendar.removeByName(name2));
+        assertFalse(calendar.removeByName(name2));
+    }
+}
