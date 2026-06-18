c7391829cc6546cd33f6390a3d7cee2a367ea086
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-18 12:26:27 +0300

    replace sout and System.err by logger.info

--- src/main/java/hexlet/pet/App.java
+++ src/main/java/hexlet/pet/App.java
@@ -9,6 +9,7 @@ import picocli.CommandLine.Option;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Command(
         name = "EventCalendar",
@@ -17,6 +18,7 @@ import java.util.List;
         description = "An events calendar where you can add events and see when the next one is"
 )
 public class App implements  Runnable {
+    Logger logger =  Logger.getLogger(App.class.getName());
 
     @Option(names = {"-a", "--add"}, description = "add new event")
     private boolean add;
@@ -76,25 +78,25 @@ public class App implements  Runnable {
             switch (mode()) {
                 case "add":
                     if (name == null || date == null) {
-                        System.err.println("Error: --add requires --name and --date");
+                        logger.info("Error: --add requires --name and --date");
                         System.exit(1);
                     }
                     LocalDateTime dateTime = date.atStartOfDay();
                     calendar.add(name, dateTime);
-                    System.out.println("Event added: " + name + " on " + dateTime);
+                    logger.info("Event added: " + name + " on " + dateTime);
                     break;
 
                 case "clear":
                     calendar.clear();
-                    System.out.println("All events are removed.");
+                    logger.info("All events are removed.");
                     break;
 
                 case "removeId":
                     boolean removed = calendar.removeById(removeId);
                     if (removed) {
-                        System.out.println("Event with ID " + removeId + " removed.");
+                        logger.info("Event with ID " + removeId + " removed.");
                     } else  {
-                        System.out.println("Event with ID " + removeId + " not found.");
+                        logger.info("Event with ID " + removeId + " not found.");
                         System.exit(1);
                     }
                     break;
@@ -102,9 +104,9 @@ public class App implements  Runnable {
                 case "removeName":
                     boolean removedName = calendar.removeByName(removeName);
                     if (removedName) {
-                        System.out.println("Event with name " + removeName + " removed.");
+                        logger.info("Event with name " + removeName + " removed.");
                     } else {
-                        System.out.println("Event with name " + removeName + " not found.");
+                        logger.info("Event with name " + removeName + " not found.");
                         System.exit(1);
                     }
                     break;
@@ -114,31 +116,31 @@ public class App implements  Runnable {
                     List<Event> upcomingEvents = calendar.getUpcoming(show);
 
                     if (!pastEvents.isEmpty()) {
-                        System.out.println("Past " + pastEvents.size() + " events:");
+                        logger.info("Past " + pastEvents.size() + " events:");
                         for (int i = 0; i < pastEvents.size(); i++) {
                             Event pastEvent = pastEvents.get(i);
-                            System.out.println(i + 1 + ". " + pastEvent.toString());
+                            logger.info(i + 1 + ". " + pastEvent.toString());
                         }
                     } else {
-                        System.out.println("No past events found.");
+                        logger.info("No past events found.");
                     }
 
                     if (!upcomingEvents.isEmpty()) {
-                        System.out.println("Upcoming " + upcomingEvents.size() + " events:");
+                        logger.info("Upcoming " + upcomingEvents.size() + " events:");
                         for (int i = 0; i < upcomingEvents.size(); i++) {
                             Event upcomingEvent = upcomingEvents.get(i);
-                            System.out.println(i + 1 + ". " + upcomingEvent.toString());
+                            logger.info(i + 1 + ". " + upcomingEvent.toString());
                         }
                     } else  {
-                        System.out.println("No upcoming events found.");
+                        logger.info("No upcoming events found.");
                     }
                     break;
 
                 default:
-                    System.err.println("Error: invalid command line options");
+                    logger.info("Error: invalid command line options");
             }
         } catch (Exception e) {
-            System.err.println("Error " + e.getMessage());
+            logger.info("Error " + e.getMessage());
             System.exit(1);
         }
     }
