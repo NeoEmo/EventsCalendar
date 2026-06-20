7d79c4fe41c9f29be54ea63567103a8ee34b1946
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-20 21:02:34 +0300

    Update AppTest.java

--- src/test/java/hexlet/pet/AppTest.java
+++ src/test/java/hexlet/pet/AppTest.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class AppTest {
     private Path tempFile;
