2583433f78d383bf8a773ddea06fb375c307650a
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-24 22:08:55 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,14 @@ java {
 
 repositories {
     mavenCentral()
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/NeoEmo/EventsCalendar")
+        credentials {
+            username = System.getenv("GITHUB_ACTOR")
+            password = System.getenv("GITHUB_TOKEN")
+        }
+    }
 }
 
 dependencies {
